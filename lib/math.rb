8dd2eae34de24e449db79ce680af080a7fb0e542
Riley Moore <rilemoore@gmail.com>
2019-09-16 11:18:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,23 @@ return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+return num1 - num2
 end
 
 def division(num1, num2)
-
+return num1 / num2
 end
 
 def multiplication(num1, num2)
-
+return num1 * num2
 end
 
 def modulo(num1, num2)
-
+return num1 % num2
 end
 
-def square_root(num)
 
+
+def square_root(num)
+return num.sqrt
 end
